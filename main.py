60c2371e2eb47e14bc4d281ba3b80173736dab86
Pouria Shafiee <shafieeofficial@gmail.com>
2020-10-22 19:49:57 +0330

    First Commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from rules import url
+import requests
+import json
+from random import choices
+import string
+
+
+def create_phone_fake(country_code_input="IR", area_input="khozestan"):
+    response = requests.get(url)
+    if response.status_code == 200:
+        list_json = json.loads(response.text)
+        list_result = list_json['result']
+        if check_validation(list_result, country_code_input, area_input):
+            list_country = list_result[country_code_input]
+            country_code = list_country['country_code']  # ***
+            area_code = list_country['area_code'][area_input]
+            return country_code + area_code + ''.join(choices(string.digits, k=7))
+        else:
+            return "Invalid"
+
+
+def check_validation(list_result, country_code_input, area_input):
+    temp = False
+    for i in list_result:
+        if i == country_code_input:
+            for j in list_result[str(i)]:
+                for jj in list_result[str(i)][j]:
+                    if jj == area_input:
+                        temp = True
+                    else:
+                        temp = False
+    return temp
+
+
+if __name__ == '__main__':
+    print(create_phone_fake(area_input='texas', country_code_input="USA"))
+    print(create_phone_fake(area_input='texas', country_code_input="BBBB"))
---
+++ rules.py
@@ -0,0 +1,26 @@
+URL_PATH = "https://api.jsonbin.io/b/"
+API_KEY = "5f913e49adfa7a7bbea6b4a3/1"
+url = URL_PATH + API_KEY
+
+'''
+@Sample response : 
+{
+"result": {
+"IR": {
+"country_code": "+98",
+"area_code": {
+"tehran": "912",
+"fars": "917",
+"khozestan": "916"
+}
+},
+"USA": {
+"country_code": "+1",
+"area_code": {
+"washington": "206",
+"texas": "210"
+}
+}
+}
+}
+'''
