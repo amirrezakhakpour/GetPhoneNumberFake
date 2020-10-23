20cf68456ce8561bcae470f00e5b1e7a59f22f03
Pemidi <rashidi.peyman@gmail.com>
2020-10-23 18:28:31 +0330

    code cleaned, check validator converted to python error handling

--- main.py
+++ main.py
@@ -5,7 +5,17 @@ from random import choices
 import string
 
 
-def phone_number_generator(country_code="IR", area="khozestan"):
+def create_phone_number(country_code="IR", local_code="khozestan"):
+    """[summary]
+    an function that get a country and local code, an finally
+    generate a random Phone number
+    Args:
+        country_code (str, optional): [a country code]. Defaults to "IR".
+        local_code (str, optional): [a local code]. Defaults to "khozestan".
+
+    Returns:
+        [string]: [a random generated phone number]
+    """
 
     response = requests.get(url)
 
@@ -13,34 +23,21 @@ def phone_number_generator(country_code="IR", area="khozestan"):
         parsed_data = json.loads(response.text)
         data = parsed_data['result']
         
-        if check_validation(data, country_code, area):
+        try:
             list_country = data[country_code]
             country_code = list_country['country_code']  # ***
-            area_code = list_country['area_code'][area]
+            area_code = list_country['area_code'][local_code]
             generated_number = ''.join(choices(string.digits, k=7))
             return country_code + area_code + generated_number
-        else:
-            return "Invalid"
 
+        except KeyError:
+            print('invalid input! try again')
+            
 
-def check_validation(data, country_code, area):
-    temp = False
-
-    for i in data:
-
-        if i == country_code:
-
-            for j in data[str(i)]:
-
-                for jj in data[str(i)][j]:
-                    
-                    if jj == area:
-                        temp = True
-                    else:
-                        temp = False
-    return temp
-
+        
 
 if __name__ == '__main__':
-    print(phone_number_generator(area='texas', country_code="USA"))
-    print(phone_number_generator(area='texas', country_code="BBBB"))
+
+    generated_number = create_phone_number(local_code='texas', country_code="USA") 
+    print(generated_number)
+    
